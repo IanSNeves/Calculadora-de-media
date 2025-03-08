ec6f9fdd8f531cbfed5aaf6274471fde367d1fa2
Iannvs <iansneves.bdo@gmail.com>
2025-03-07 23:42:01 -0300

    Terceiro commit: main.js - Foi criado a logica para o calculo da media final, alem das funcoes que exibe o resultado do calculo e a situacao do aluno

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ const atividadeInput = document.querySelector("#id-atividade");
 const notaInput = document.querySelector ("#id-nota");
 const submitbutton = document.querySelector("#submit-button");
 const tabela = document.querySelector("#bodyTable");
+const notaMinima = parseFloat(prompt("digite a nota miníma para aprovação!"))
 
 // Definindo imagens de aprovado e reprovado
 const imgAprovado = `<img src="./images/aprovado.png"></img>`
 const imgReprovado = `<img src="./images/reprovado.png"></img>`
 
+// Exibindo resultado final
+    const alunoAprovado = '<span class="aprovado">Aprovado</span>'
+    const alunoReprovado = '<span class="reprovado">Reprovado</span>'
+
 //Criando uma lista que ira armazenar as atividades para que nao haja duplicidade
 const atividadesRegistradas = new Set();
-
+const notasRegistradas = [];
 
 // Funcoes de validacao
 // Funcao para limpar os campos
@@ -40,9 +45,24 @@ alert ("Por favor, preencha os campos corretamente!")
         novaLinha.innerHTML = `
         <td>${atividadeValue}</td>
         <td>${notaValue}
-        <td>${notaValue >= 6 ? imgAprovado : imgReprovado}</td>`;
+        <td>${notaValue >= notaMinima ? imgAprovado : imgReprovado}</td>`;
         return novaLinha
 }
+
+// Funcao para calculo da media final
+    function calculoDeMedia(){
+        if (notasRegistradas.length === 0) return 0;
+        const somaDasNotas = notasRegistradas.reduce((total, nota) => total + nota, 0);
+        return somaDasNotas / notasRegistradas.length; 
+    }
+
+    function Resultado(){
+        const mediaFinal = calculoDeMedia();
+        console.log(mediaFinal);
+
+        document.querySelector("#resultado-media").innerHTML = mediaFinal;
+        document.querySelector("#situacao-aluno").innerHTML = mediaFinal >= notaMinima ? alunoAprovado : alunoReprovado;
+    }
 
 // Criando um evento para quando o Botao Adicionar for clicado
 submitbutton.addEventListener('click', (e) => {
@@ -54,13 +74,19 @@ submitbutton.addEventListener('click', (e) => {
 
     if (validarDados(atividadeValue, notaValue)) {
         atividadesRegistradas.add(atividadeValue);// adiciona a atividade a lista de atividades registradas
+        notasRegistradas.push(notaValue) // adiciona a nota a lista de notas registradas
 
         const novaLinha = criarLinha(atividadeValue, notaValue);
         tabela.appendChild(novaLinha); // Cria uma nova linha 
     
         limparCampos();
 
+        calculoDeMedia();
+
+        Resultado();
+
         console.log(atividadesRegistradas);
+        
     }
 
 });
